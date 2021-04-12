214a11868a5861f5b541f4b4e99028cd654fa733
saunaik19 <saunaik19@gmail.com>
2021-04-12 21:18:12 +0530

    First commit

---
+++ src/main/java/com/saurabh/filereader/LargeFileReaderApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.filereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeFileReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeFileReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/filereader/LargeFileReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.filereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeFileReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
